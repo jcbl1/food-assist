ad21eee90051c76e2f5ca802d6a18797701b8b8f
Jacob Lin <linwei2024@whut.edu.cn>
2025-03-31 11:47:06 +0800

    update

--- src/components/ListShowcase.vue
+++ src/components/ListShowcase.vue
@@ -1,19 +1,79 @@
 <script setup lang="ts">
-import ListShowcaseItem from './ListShowcaseItem.vue';
-import { ref } from 'vue';
-const count = ref(1);
-const loadMoreItems = () => {
-    console.log('loading more items');
-    count.value += 5;
-};
+import ListShowcaseItem, { type FoodData } from './ListShowcaseItem.vue';
+import { ref, onBeforeMount } from 'vue';
+import axios from 'axios';
+
+const queryFoodData = async (current: number) => {
+    const queryUrl = 'https://furina.yelr.de/food/list?current='+current
+    // console.log("queryUrl: %s", queryUrl)
+    const response = await axios.get<FoodData[]>(queryUrl)
+    // const response = await axios.get<FoodData[]>('http://localhost:10240/food/list')
+    return response.data;
+}
+
+const log = (message: string) => {
+    console.log(message)
+    // LogPrint(message)
+}
+
+// export const useQueryFoodData = queryFoodData
+
+const foodData = ref<FoodData[]>([]);
+const count = ref(0);
+const hasMore = ref(true);
+const loading = ref(false)
+const loadMoreItems = async () => {
+    loading.value=true
+    log("loading more items")
+    let resCount = 0
+    // const response = await queryFoodData(count.value/5+1)
+    // const resCount=response.length
+    // foodData.value=foodData.value.concat(response)
+    // count.value+=resCount
+    // if (resCount<5){
+    //     hasMore.value=false
+    // }
+    await queryFoodData(count.value/5+1).then((response)=>{
+        resCount = response.length
+        // console.log("count: %d", count.value)
+        // console.log("count/5+1: %d", count.value/5+1)
+        // console.log("resCount: %d", resCount)
+        // console.log(response)
+        foodData.value=foodData.value.concat(response)
+        // console.log(foodData.value)
+        count.value += resCount
+    }).catch((error)=>{
+        if (axios.isAxiosError(error)){
+            log("error: " + error.message)
+            if (error.request){
+                log("method: "+error.config?.method)
+                log("url: "+error.config?.url)
+                log("headers: "+error.config?.headers)
+            }
+        }else{
+            log("error: " + error)
+        }
+    }).finally(()=>{
+        if (resCount<5){
+            hasMore.value = false
+        }
+    })
+    loading.value=false
+}
 </script>
 
 <template>
-    <el-container v-infinite-scroll="loadMoreItems" direction="vertical">
+    <el-container v-infinite-scroll="loadMoreItems" :infinite-scroll-disabled="!hasMore || loading"
+        :infinite-scroll-delay="200" :infinite-scroll-immediate="true" direction="vertical">
         <!-- <el-row v-for="n in 16" justify="space-around" align="middle"> -->
         <el-row v-for="i in count" :key="i" justify="space-around" align="middle">
             <el-col :xs="23" :sm="18" :md="14" :lg="10" :xl="6">
-                <ListShowcaseItem />
+                <ListShowcaseItem>
+                    <template #title>{{ foodData[i-1].title }}</template>
+                    <template #rating>{{ foodData[i-1].rating }}分</template>
+                    <template v-if="foodData[i-1].salesMonthly" #salesMonthly>{{ foodData[i-1].salesMonthly
+                        }}人已点</template>
+                </ListShowcaseItem>
             </el-col>
         </el-row>
     </el-container>
@@ -23,6 +83,7 @@ const loadMoreItems = () => {
 .paddingBottomMedium {
     padding-bottom: 2em;
 }
+
 .paddingTopMedium {
     padding-top: 2.6em;
 }
--- src/components/ListShowcaseItem.vue
+++ src/components/ListShowcaseItem.vue
@@ -1,4 +1,22 @@
 <script setup lang="ts">
+export interface FoodDataItem {
+    id?: number;
+    name: string;
+    description: string;
+    price: number;
+    foodDataId: number;
+}
+export interface FoodData {
+    id?: number;
+    thumbnailPath: string;
+    title: string;
+    rating: number;
+    salesMonthly: number;
+    averageCost?: number;
+    deliveryTime?: string;
+    featuredComment?: string;
+    items?: FoodDataItem[];
+}
 </script>
 <template>
     <el-container class="marginSmall">
@@ -10,7 +28,24 @@
             </el-skeleton>
         </el-aside>
         <el-main class="pd-5">
-            <el-skeleton :rows="2"/>
+            <slot></slot>
+            <h3 class="itemTitle">
+                <slot name="title">默认标题</slot>
+            </h3>
+            <el-row justify="space-between" align="middle">
+                <el-col :span="12">
+                    <el-text tag="b" type="warning" class="marginRightSmall">
+                        <slot name="rating">0.0分</slot>
+                    </el-text>
+                    <el-text>
+                        <slot name="salesMonthly">还没得人点</slot>
+                    </el-text>
+                </el-col>
+                <el-col :span="12" class="textRight">
+                    <el-text class="marginRightSmall">39分钟</el-text>
+                    <el-text>2.0km</el-text>
+                </el-col>
+            </el-row>
         </el-main>
     </el-container>
 </template>
@@ -18,7 +53,21 @@
 .pd-5 {
     padding: 10px;
 }
+
 .marginSmall {
     margin: 0.2em;
+}
+
+.itemTitle {
+    margin: 0;
+    margin-bottom: 0.2em;
+}
+
+.marginRightSmall {
+    margin-right: 0.4em;
+}
+
+.textRight {
+    text-align: right;
 }
 </style>
